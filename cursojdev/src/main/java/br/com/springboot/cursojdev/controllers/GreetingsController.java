b182e3453bf0bc6acdda177c483bb1f6c4d1767e
Leandro Martins <leandro2828@gmail.com>
2024-10-11 15:48:58 -0300

    Projeto configurado e testado

---
+++ cursojdev/src/main/java/br/com/springboot/cursojdev/controllers/GreetingsController.java
@@ -0,0 +1,39 @@
+package br.com.springboot.cursojdev.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.springboot.cursojdev.model.Usuario;
+import br.com.springboot.cursojdev.repository.UsuarioRepository;
+
+/**
+ *
+ * A sample greetings controller to return greeting text
+ */
+@RestController
+public class GreetingsController {
+    /**
+     *
+     * @param name the name to greet
+     * @return greeting text
+     */
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String greetingText(@PathVariable String name) {
+    	
+    	Usuario usuario = new Usuario();
+    	usuario.setNome(name);
+    	usuarioRepository.save(usuario);
+    	
+        return "Hello " + name + "!";
+    }
+}
