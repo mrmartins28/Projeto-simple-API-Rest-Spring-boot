a0bf98475d0a828844f47fb4d06644eb5264388b
Leandro Martins <leandro2828@gmail.com>
2024-10-12 15:31:08 -0300

    Subindo o projeto

--- cursojdev/src/main/java/br/com/springboot/cursojdev/controllers/GreetingsController.java
+++ cursojdev/src/main/java/br/com/springboot/cursojdev/controllers/GreetingsController.java
@@ -1,10 +1,20 @@
 package br.com.springboot.cursojdev.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +36,69 @@ public class GreetingsController {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+
+	@GetMapping(value = "listartodos")
+	@ResponseBody
+	public ResponseEntity<List<Usuario>> listaUsuario(){
+		
+		List<Usuario> usuarios = usuarioRepository.findAll();
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "salvar")
+	@ResponseBody
+	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){
+		
+		Usuario user = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+	}
+	
+	
+	@DeleteMapping(value = "delete")
+	@ResponseBody
+	public ResponseEntity<String> delete(@RequestParam Long id){
+		
+		usuarioRepository.deleteById(id);
+		
+		return new ResponseEntity<String>("User deletado", HttpStatus.OK);
+	}
+	
+	@GetMapping("buscarPorId")
+	@ResponseBody
+	public ResponseEntity<Usuario> consultarUserId(@RequestParam(name = "iduser") Long iduser){
+		
+		Usuario user;
+		
+		user = usuarioRepository.findById(iduser).get();
+		
+		return new ResponseEntity<Usuario>(user , HttpStatus.OK);
+		
+	}
+	@PutMapping("atualizaruser")
+	@ResponseBody
+	public ResponseEntity<?> atualizarUser(@RequestBody Usuario usuario){
+		 
+		if(usuario.getId() == null) {
+			
+			return new ResponseEntity<String>("Id não pode ser nulo", HttpStatus.OK);
+		}
+		
+		Usuario user = usuarioRepository.saveAndFlush(usuario);
+		
+		return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+	}
+	
+	@GetMapping("buscapornome")
+	@ResponseBody
+	public ResponseEntity<List<Usuario>> buscarPorNome(@RequestParam String nome){
+		
+		List<Usuario> users = usuarioRepository.consultaPorNome(nome.trim().toUpperCase());
+		
+		return new ResponseEntity<List<Usuario>>(users, HttpStatus.OK);
+	}
+	
+    @RequestMapping(value = "/mostranome/{name}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String greetingText(@PathVariable String name) {
     	
--- cursojdev/src/main/java/br/com/springboot/cursojdev/repository/UsuarioRepository.java
+++ cursojdev/src/main/java/br/com/springboot/cursojdev/repository/UsuarioRepository.java
@@ -1,11 +1,18 @@
 package br.com.springboot.cursojdev.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.springboot.cursojdev.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	@Query(value = "select u from Usuario u where upper(trim(u.nome)) like %?1%")
+	public List<Usuario> consultaPorNome(@Param(value = "parmnome") String paramnome);
 
 }
